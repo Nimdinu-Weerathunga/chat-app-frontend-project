1585f3345fac21f8d60e09fa9d8fb9cc443067c3
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-02-11 12:46:26 +0530

    initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiCKx6gaW7MgFIIqL7O8Rx4j0kfGNEDqI",
+  authDomain: "chat-app-75-e0c7a.firebaseapp.com",
+  projectId: "chat-app-75-e0c7a",
+  storageBucket: "chat-app-75-e0c7a.firebasestorage.app",
+  messagingSenderId: "791036443952",
+  appId: "1:791036443952:web:242880c503b7af86da1cf5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
