c2402a1e1c7ca80cd0d7cb2572a6475fe7e9e256
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-04-22 13:50:20 +0530

    connect to the updateprofile path

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -8,30 +8,33 @@ const storageRef = ref(storage, `images/${Date.now() + file.name}`);
 
 const uploadTask = uploadBytesResumable(storageRef, file);
 
-uploadTask.on('state_changed', 
-  (snapshot) => {
+return new promise((resolve,reject)=>{
+    uploadTask.on('state_changed', 
+        (snapshot) => {
+      
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        }, 
+        (error) => {
+      
+        }, 
+        () => {
+      
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            return(downloadURL)
+          });
+        }
+      );
+})
 
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-    }
-  }, 
-  (error) => {
-
-  }, 
-  () => {
-
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      resolve(downloadURL)
-    });
-  }
-);
 }
 
 
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -3,8 +3,12 @@ import'./ProfileUpdate.css'
 import assets from '../../assets/assets'
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from '../../config/firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Upload from '../../lib/upload';
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 
 const ProfileUpdate = () => {
     
@@ -14,13 +18,46 @@ const ProfileUpdate = () => {
     const [bio,setBio] = useState("");
     const [uid,setUid] = useState("");
     const [prevImage,setPrevImage] = useState("");
+    const {setUserData} = useContext(AppContext)
+
+    const ProfileUpdate = async (event) => {
+        event.preventDefault();
+        try {
+
+            if (!prevImage && !image) {
+                toast.error("Upload profile picture")
+            }
+            const docRef = doc(db,'users',uid);
+            if (image) {
+                const imgURL = await Upload(image);
+                setPrevImage(imgURL);
+                await updateDoc (docRef,{
+                    avatar:imgURL,
+                    bio:bio,
+                    name:name
+                })
+            }
+            else{
+                await updateDoc (docRef,{
+                    bio:bio,
+                    name:name
+                })
+            }  
+            const snap = await getDoc(docRef); 
+            setUserData(snap.data());
+            navigate('/chat');
+        } catch (error) {
+            console.error(error);
+            toast.error(error.message);
+        }
+    }
 
     useEffect(()=>{
         onAuthStateChanged(auth,async (user)=>{
             if (user) {
                 setUid(user.uid)
                 const docRef = doc(db,"user",user.uid);
-                const docSnap = getDoc(docRef);
+                const docSnap = await getDoc(docRef);
                 if (docSnap.data().name) {
                     setName(docSnap.data().name);
                 }
@@ -41,7 +78,7 @@ const ProfileUpdate = () => {
   return (
     <div className='profile'>
         <div className="profile-container">
-            <form>
+            <form onSubmit={ProfileUpdate}>
                 <h3>Profile Details</h3>
                 <label htmlFor="avatar">
                     <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
