7ec2883130b3d205b4c62b07e7ed7c2e43b420ce
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-04-24 11:37:59 +0530

    almost done leftSidebar.js

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './LeftSidebar.css'
 import assets from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../../config/firebase'
 
 const LeftSide = () => {
 
   const navigate = useNavigate();
+  
+
+  const inputHandler = async (e) => {
+    try {
+      const input = e.target.value;
+      const userRef = collection(db,'users');
+      const q = qurey(useRef,where("username","==",input.toLowerCase()));
+      const qureySnap = await getDocs(q);
+      if(!querySnap.empty)
+      {
+        console.log(qureySnap.docs[0].data());
+        
+      }
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <div className='ls'>
@@ -23,7 +42,7 @@ const LeftSide = () => {
          </div>
          <div className="ls-search">
           <img src={assets.search_icon} alt="" />
-          <input type="text" placeholder='Search here..' />
+          <input onChange={inputHandler} type="text" placeholder='Search here..' />
          </div>
       </div>
       <div className="ls-list">
