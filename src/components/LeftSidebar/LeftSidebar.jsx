5c3fdfdeef2b11fce7b3f6cb756c492e2c1eee10
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-04-24 16:25:20 +0530

    Difficult to do the stuff😒

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -2,14 +2,15 @@ import React, { useContext, useRef, useState } from 'react'
 import './LeftSidebar.css'
 import assets from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import { arrayUnion, collection, doc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore'
 import { db } from '../../config/firebase'
 import { AppContext } from '../../context/AppContext'
+import { toast } from 'react-toastify'
 
 const LeftSide = () => {
 
   const navigate = useNavigate();
-  const {userData} = useContext(AppContext);
+  const { userData,chatsData } = useContext(AppContext);
   const [user,setUser] = useState(null);
   const [showSearch,setShowSearch] =useState(false); 
 
@@ -21,9 +22,16 @@ const LeftSide = () => {
         const userRef = collection(db,'users');
         const q = qurey(useRef,where("username","==",input.toLowerCase()));
         const qureySnap = await getDocs(q);
-        if(!querySnap.empty && querySnap.docs[0].data().id !== userData.id)
-        {
-          setUser(querySnap.docs[0].data());
+        if(!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          let userExist = false
+          chatsData.map((user)=>{
+             if (user.rId === querySnap.docs[0].data().id) {
+              userExist = true;
+             }
+          })
+          if (userExist) {
+            setUser(querySnap.docs[0].data());
+          }
           
         } 
         else{
@@ -39,7 +47,39 @@ const LeftSide = () => {
   }
 
   const addChat = async () => {
+    const messagesRef = collection(db,"message");
+    const chatsRef = collection(db,"chats");
+    try {
+      const newMessageRef = doc(messagesRef);
+      await setDoc(newMessageRef,{
+        createAt:serverTimestamp(),
+        message:[]
+      })
 
+      await updateDoc(doc(chatsRef,user.id),{
+        chatsData:arrayUnion({
+          messageId:newMessageRef.id,
+          lastMessage:"",
+          rId:userData.id,
+          updatedAt:Date.now(),
+          messageSeen:true     
+        })
+      })
+
+      
+      await updateDoc(doc(chatsRef,userData.id),{
+        chatsData:arrayUnion({
+          messageId:newMessageRef.id,
+          lastMessage:"",
+          rId:userData.id,
+          updatedAt:Date.now(),
+          messageSeen:true     
+        })
+      })
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error)
+    }
   }
 
   return (
